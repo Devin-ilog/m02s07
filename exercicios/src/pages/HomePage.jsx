3420e51604b91df3096d6fa47a4d1d137db9e93d
romeuic <casarottori@gmail.com>
2023-03-03 21:37:50 -0300

    adiciona exercicio 5

--- exercicios/src/pages/HomePage.jsx
+++ exercicios/src/pages/HomePage.jsx
@@ -1,15 +1,44 @@
-import CourseCard from "../components/CourseCard";
+import CourseList from "../components/CourseList";
+
+const listaDeCursos = [
+  {
+    id: 635,
+    imageUrl: "/logo.png",
+    name: "React Js",
+    category: "front-end",
+    description: "Curso sobre React Js",
+    duration: 6,
+  },
+  {
+    id: 873,
+    imageUrl: "/logo.png",
+    name: "React com TS",
+    category: "front-end",
+    description: "Curso sobre React com TS",
+    duration: 6,
+  },
+  {
+    id: 935,
+    imageUrl: "/logo.png",
+    name: "React Router",
+    category: "front-end",
+    description: "Curso sobre React com TS",
+    duration: 6,
+  },
+  {
+    id: 333,
+    imageUrl: "/logo.png",
+    name: "Context API",
+    category: "front-end",
+    description: "Curso sobre React com TS",
+    duration: 6,
+  },
+];
 
 function HomePage() {
   return (
     <div className="homePage">
-      <CourseCard
-        imageUrl="/logo.png"
-        name="React Js"
-        category="front-end"
-        description="Curso sobre React Js"
-        duration={6}
-      />
+      <CourseList list={listaDeCursos} />
     </div>
   );
 }
